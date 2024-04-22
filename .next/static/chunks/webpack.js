ca03e0b6655354980ada636d92b22063121f0617
Genna <gennamweber@gmail.com>
2024-04-22 15:24:38 -0400

    language switcher functional

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -163,7 +163,7 @@
 /******/ 		// This function allow to reference async chunks
 /******/ 		__webpack_require__.u = function(chunkId) {
 /******/ 			// return url for filenames based on template
-/******/ 			return undefined;
+/******/ 			return "static/chunks/" + chunkId + ".js";
 /******/ 		};
 /******/ 	}();
 /******/ 	
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "34fd4b1013fde373"; }
+/******/ 		__webpack_require__.h = function() { return "d7896b4c7d3c787d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */

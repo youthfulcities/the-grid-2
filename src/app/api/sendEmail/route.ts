570c420808824356abbe9d0175b52667457fb8e5
Genna <gennamweber@gmail.com>
2024-06-06 12:00:04 -0400

    Add console.log messags for troubleshooting email send

--- src/app/api/sendEmail/route.ts
+++ src/app/api/sendEmail/route.ts
@@ -1,13 +1,21 @@
 /* eslint-disable import/prefer-default-export */
 
-import { NextRequest, NextResponse } from "next/server";
-import AWS from "aws-sdk";
+import AWS from 'aws-sdk';
+import { NextRequest, NextResponse } from 'next/server';
 
 const ses = new AWS.SES({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: "ca-central-1" 
+  region: 'ca-central-1',
 });
+
+if (process.env.AWS_ACCESS_KEY_ID === undefined) {
+  console.log('AWS_ACCESS_KEY_ID is undefined');
+}
+
+if (process.env.AWS_SECRET_ACCESS_KEY === undefined) {
+  console.log('AWS_SECRET_ACCESS_KEY is undefined');
+}
 
 export async function POST(req: NextRequest) {
   const { email, firstName, lastName, message } = await req.json();
@@ -15,19 +23,19 @@ export async function POST(req: NextRequest) {
   // Validate the input
   if (!email || !firstName || !lastName || !message) {
     return NextResponse.json(
-      { error: "All fields are required." },
+      { error: 'All fields are required.' },
       { status: 400 }
     );
   }
 
   const params = {
-    Source: "info@youthfulcities.com",
+    Source: 'info@youthfulcities.com',
     Destination: {
-      ToAddresses: ["info@youthfulcities.com"],
+      ToAddresses: ['info@youthfulcities.com'],
     },
     Message: {
       Subject: {
-        Data: "Contact Form Submission",
+        Data: 'Contact Form Submission',
       },
       Body: {
         Text: {
@@ -39,9 +47,12 @@ export async function POST(req: NextRequest) {
 
   try {
     await ses.sendEmail(params).promise();
-    return NextResponse.json({ success: "Email sent successfully." }, {status: 200});
+    return NextResponse.json(
+      { success: 'Email sent successfully.' },
+      { status: 200 }
+    );
   } catch (err: any) {
-    console.error("Error sending email:", err);
+    console.error('Error sending email:', err);
     return NextResponse.json(
       { error: `Email could not be sent: ${err.message}` },
       { status: 500 }

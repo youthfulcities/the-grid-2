83039bacc96a40f025c735c63c68297f691c4621
Genna <gennamweber@gmail.com>
2024-06-25 17:20:59 -0400

    Update tooltip to always be correct distance above button

--- next.config.js
+++ next.config.js
@@ -1,3 +1,18 @@
+const withTM = require('next-transpile-modules')([
+  '@mui/material',
+  '@mui/system',
+]);
+
+module.exports = withTM({
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@mui/styled-engine': '@mui/styled-engine-sc',
+    };
+    return config;
+  },
+});
+
 module.exports = {
   compiler: {
     styledComponents: true,

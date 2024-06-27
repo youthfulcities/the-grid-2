68953cbab3e9c70bff59a385ee75cde2284e4a23
Genna <gennamweber@gmail.com>
2024-06-27 17:04:14 -0400

    Adds privacy policy and terms and conditions

---
+++ src/app/[lng]/terms/page.tsx
@@ -0,0 +1,299 @@
+'use client';
+
+import Container from '@/app/components/Background';
+import { Heading, Text, View } from '@aws-amplify/ui-react';
+
+const page = () => {
+  return (
+    <Container>
+      <View className='container section-padding'>
+        <Heading marginBottom='small' level={1}>
+          Terms and <span className='highlight'>Conditions</span>
+        </Heading>
+        <Text>Last updated 06-27-2024</Text>
+        <a href='https://docs.google.com/document/d/e/2PACX-1vSd5s9w7YNbnt8i3MxpEF7X6mdqzsg-Xu3Wfzcgqa6foZmanWKyM8aeoJDAGUUmKfXLTj6hP2TgPetI/pub'>
+          The most up-to-date version of this policy can be found here.
+        </a>
+        <Heading className='padding' level={2}>
+          1. Terms and conditions of use of site
+        </Heading>
+        <View className='inner-container'>
+          <Text>
+            This web site (the “site”) is provided solely for informational
+            purposes by Toboggan Flats, a Youthful Cities (“Youthful Cities”)
+            venture, and its use by you is conditional on your acceptance of the
+            terms and conditions of use set forth below (the “Terms and
+            Conditions”). By visiting or using this site, including without
+            limitation to access, use, and/or download messages, information,
+            data, text, software or images, or other content from this site
+            (collectively the “content”), or to send messages, information,
+            data, text, software or images, or other Content to the site, you
+            agree on your own behalf, and on behalf of any entity on whose
+            behalf you may act, to accept and abide by the Terms and Conditions
+            for each use of and each visit to this site. If you do not agree to
+            abide by these Terms and Conditions, do not use this site.
+          </Text>
+          <Text>
+            Youthful Cities has the right, in its sole discretion, to add to,
+            remove, modify or otherwise change any part of the Terms and
+            Conditions, in whole or in part, at any time. Changes will be
+            effective when notice of such change is posted at the site. If any
+            term, condition or any change thereto is not acceptable to you, you
+            must discontinue your use of this site immediately. Your continued
+            use of this site after any such changes are posted will constitute
+            acceptance of those changes. These Terms and Conditions apply
+            exclusively to your use of this site and do not alter the terms or
+            conditions of any other agreement you may have with Youthful Cities.
+          </Text>
+          <Text>
+            Youthful Cities may, at any time without notice or liability, and
+            for any reason whatsoever, terminate, change, suspend or discontinue
+            any aspect of this site, including (i) changing the availability of,
+            restricting access to, or imposing limits on any or all features or
+            services on, or links to, the site (ii) adding, removing or changing
+            any fees or charges for use of the site or any features thereof;
+            (iii) removing, adding, modifying or otherwise changing any Content
+            on this site. Youthful Cities reserves the right, in its sole
+            discretion, to correct any errors or omissions in any portion of
+            this site at any time without notice, but confirms that it has no
+            duty to do so.
+          </Text>
+        </View>
+        <Heading className='padding' level={2}>
+          2. Limited license
+        </Heading>
+        <View as='section' className='inner-container'>
+          <Text>
+            You are granted a Canadian, non–exclusive, non–transferable,
+            non–sub–licensable, revocable, limited license to display on your
+            computer, print and download and use the content of this site for
+            informational purposes only and solely for your own personal or
+            internal company use. You may not modify any Content that you use.
+            You must include with any display or copy of the content its
+            copyright notice (that is, the line at the bottom of this page). No
+            other use is permitted.
+          </Text>
+          <Text>You may not:</Text>
+          <ol>
+            <li>Resell any content;</li>
+            <li>
+              Include any content in or with any product that you create or
+              distribute;
+            </li>
+            <li>Copy any content onto your own or any other website; or</li>
+            <li>
+              Use this site in any manner that could damage, disable,
+              overburden, impair, interfere with the security of, negatively
+              affect the functioning of, or otherwise abuse, this site or any
+              services, system resources, accounts, servers, networks,
+              affiliated or linked sites, connected to or accessible through
+              this site (including without limitation uploading, posting or
+              otherwise transmitting on this site computer viruses, Trojan
+              horses, worms or other files or computer programs which are
+              potentially harmful, disruptive or destructive or that may impose
+              an unreasonable or disproportionately large load on this site’s
+              infrastructure; or using any robot, spider or other automatic
+              program or device, or manual process to monitor, copy, summarize,
+              or otherwise extract information from this site or the Content in
+              whole or in part).
+            </li>
+          </ol>
+          <Text>
+            In addition, you agree that you are solely responsible for actions
+            and communications undertaken or transmitted in the course of your
+            usage of this site, and that you will comply with all laws that
+            apply or may apply to your use of or activities on this site or in
+            respect of the Content. Youthful Cities will investigate occurrences
+            which may involve violations of such laws, and may involve, and
+            co–operate with, law enforcement authorities in prosecuting users
+            who are involved in such violations. Youthful Cities reserves the
+            right at all times to disclose any information regarding your usage
+            of the site as necessary to satisfy any law, regulation or
+            governmental request.
+          </Text>
+        </View>
+        <Heading className='padding' level={2}>
+          3. Proprietary rights
+        </Heading>
+        <View className='inner-container'>
+          <Text>
+            The Content is protected by Canadian and worldwide copyright laws
+            and treaty provisions. Any unauthorized copying, redistribution,
+            reproduction or modification of the Content by any person may be a
+            violation of trade–mark and/or copyright laws and could subject such
+            person to legal action. You agree to comply with all copyright laws
+            worldwide in your use of this site and to prevent any unauthorized
+            copying of the Content.
+          </Text>
+          <Text>
+            Certain names, graphics, logos, icons, designs, words, titles and
+            phrases on this site constitute trade–marks, trade names, trade
+            dress and associated products and services of Youthful Cities or its
+            affiliates (the “Marks”), and are protected in Canada and
+            internationally and their display on this site does not convey or
+            create any licence or other rights in the Marks. Any use of any of
+            same, in whole or in part, without prior written authorization of
+            Youthful Cities is strictly prohibited.
+          </Text>
+          <Text>
+            Other trade–marks, trade names, trade dress and associated products
+            and services mentioned on this Site may be the trademarks of their
+            respective owners. The display of these trade–marks, trade names,
+            trade dress and associated products and services on this site does
+            not convey or create any licence or other rights in these trademarks
+            or trade names. Any unauthorized use of them is strictly prohibited.
+          </Text>
+        </View>
+        <Heading className='padding' level={2}>
+          4. Linked sites
+        </Heading>
+        <View className='inner-container'>
+          <Text>
+            Certain links on this site may take you to other websites. Youthful
+            Cities provides these links only as a convenience.
+          </Text>
+          <Text>
+            Youthful Cities is not responsible for the content of any such
+            linked pages. Youthful Cities makes no representation or warranty
+            regarding, and does not endorse, any linked websites, the
+            information appearing thereon or any of the products or services
+            described. If you use these sites, you will leave this site.
+          </Text>
+          <Text>
+            If you decide to visit any linked site, you do so at your own risk
+            and it is your responsibility to take all protective measures to
+            guard against viruses and other destructive elements.
+          </Text>
+          <Text>
+            Youthful Cities welcomes links to this site. However, you agree that
+            if you want to link to this site your website shall not:
+          </Text>
+          <ol>
+            <li>
+              create frames around any part of this site or use other techniques
+              that alter the visual presentation of this site;
+            </li>
+            <li>
+              imply that Youthful Cities is endorsing you or any other person,
+              or your or such other person’s products or services;
+            </li>
+            <li>
+              imply an affiliation between you or any other person, or your or
+              such other person’s products or services, and Youthful Cities
+              without the prior written consent of Youthful Cities;
+            </li>
+            <li>
+              misrepresent the relationship of you or any other person with
+              Youthful Cities or present false, misleading or otherwise damaging
+              information or impressions about Youthful Cities or any of its
+              products or services; or
+            </li>
+            <li>
+              contain materials that may be interpreted as distasteful, harmful,
+              offensive, infringing or inaccurate.
+            </li>
+          </ol>
+        </View>
+        <Heading className='padding' level={2}>
+          5. Disclaimer
+        </Heading>
+        <View className='inner-container'>
+          <Text>
+            Youthful Cities makes reasonable efforts to ensure that the Content
+            is accurate at the time it is posted on the site, however, Youthful
+            Cities makes no representation or warranty of any kind regarding the
+            site or the Content, including without limitation, any
+            representation or warranty regarding the accuracy, reliability,
+            currency, or completeness of the Content.
+          </Text>
+          <Text>
+            Youthful Cities makes no representation or warranty that Content at
+            this site is appropriate or available for use at any locations
+            outside Canada. Accessing the Content from locations where their
+            contents are illegal is prohibited. Those who choose to access this
+            site from other locations do so on their own initiative and are
+            responsible for compliance with local laws.
+          </Text>
+          <Text>
+            You acknowledge and agree that the site (including without
+            limitation the Content) is provided on an “AS IS” basis; and that
+            any use of or reliance on the site shall be at your sole risk.
+          </Text>
+          <Text>
+            Youthful Cities and its affiliates do not make any, and expressly
+            disclaim all, representations, warranties, covenants and conditions,
+            express or implied, by operation of law or otherwise, including
+            without limitation any implied warranties and conditions of
+            merchantability, merchantable quality, or fitness for any particular
+            purpose, or non–infringement, or any implied representations or
+            warranties arising out of course of performance, course of dealing
+            or usage of trade. For greater certainty, Youthful Cities does not
+            warrant that this site will continue to operate, operate without
+            interruptions or be error–free.
+          </Text>
+        </View>
+        <Heading className='padding' level={2}>
+          6. Limitation of liability
+        </Heading>
+        <View className='inner-container'>
+          <Text>
+            You agree that in no event will Youthful Cities or any of its
+            affiliates have any responsibility or liability in connection with
+            this site for any loss or damages whatsoever, whether based on
+            contract, negligence or other legal basis, including without
+            limitation direct, indirect, special, punitive, exemplary or
+            consequential damages or other damages (including without limitation
+            damages for harm to business, loss of information or programs or
+            data, loss of profit, loss of savings, loss of revenue, loss of
+            goodwill), arising from or in connection with the use of or access
+            to, or any inconvenience, delay or loss of use of or access to, this
+            site, the content, any content of any linked web site, or failure of
+            such web sites, (including without limitation any damages suffered
+            as a result of omissions or inaccuracies in such web sites or
+            content, or the transmission of confidential or sensitive
+            information to or from such web sites) even if Youthful Cities or
+            any of its affiliates has been advised of the possibility of such
+            damage or loss or if such damage or loss was foreseeable.
+          </Text>
+        </View>
+        <Heading className='padding' level={2}>
+          7. Choice of law
+        </Heading>
+        <View className='inner-container'>
+          <Text>
+            This site is controlled, operated and administered by Youthful
+            Cities from within the Province of Ontario, Canada. By accessing
+            this site, you agree that all matters relating to the access to, or
+            use of, this site shall be governed by the laws of the Province of
+            Ontario and the federal laws of Canada applicable therein, without
+            reference to its conflict of laws principles, and that you will
+            comply with all such applicable laws.
+          </Text>
+        </View>
+        <Heading className='padding' level={2}>
+          8. General
+        </Heading>
+        <View className='inner-container'>
+          <Text>
+            The Terms and Conditions constitute the entire agreement between
+            Youthful Cities and you with respect to the subject matter hereof,
+            and supersede all communications, representations or agreements,
+            either oral or written, between Youthful Cities or its affiliates
+            and you with respect to this subject matter. If for any reason a
+            court of competent jurisdiction finds any provision of the Terms and
+            Conditions or portion thereof to be unenforceable, that provision
+            shall be enforced to the maximum extent permissible so as to effect
+            the intent of the Terms and Conditions, and the remainder of the
+            Terms and Conditions shall continue in full force and effect.
+            Youthful Cities’ failure to insist upon or enforce strict
+            performance of any provision of the Terms and Conditions or any
+            right shall not be construed as a waiver of any such provision or
+            right.
+          </Text>
+        </View>
+      </View>
+    </Container>
+  );
+};
+
+export default page;

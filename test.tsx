34042ff0066207cb2a27895fbd491944d0ccae46
Genna <gennamweber@gmail.com>
2024-07-15 16:43:08 -0400

    Add survey page

---
+++ test.tsx
@@ -0,0 +1,39 @@
+{
+  "aws_project_region": "ca-central-1",
+  "aws_mobile_analytics_app_id": "ee76912296834eccba37215797d53662",
+  "aws_mobile_analytics_app_region": "ca-central-1",
+  "Analytics": {
+    "AWSPinpoint": {
+      "appId": "ee76912296834eccba37215797d53662",
+      "region": "ca-central-1"
+    }
+  },
+  "aws_cloud_logic_custom": [
+    {
+      "name": "s3",
+      "endpoint": "https://jocbdt2gu4.execute-api.ca-central-1.amazonaws.com/staging",
+      "region": "ca-central-1"
+    }
+  ],
+  "aws_appsync_graphqlEndpoint": "https://6vw4kod24jhonofquxb5ssdjne.appsync-api.ca-central-1.amazonaws.com/graphql",
+  "aws_appsync_region": "ca-central-1",
+  "aws_appsync_authenticationType": "API_KEY",
+  "aws_appsync_apiKey": "da2-qosxnsuvmvcwffnmavymmgn62i",
+  "aws_cognito_identity_pool_id": "ca-central-1:fc266afd-e27d-470b-8a33-91a8c55b592d",
+  "aws_cognito_region": "ca-central-1",
+  "aws_user_pools_id": "ca-central-1_sIMkognot",
+  "aws_user_pools_web_client_id": "3pjo2i5tdj4vp62dc3tqm4p5g7",
+  "oauth": {},
+  "aws_cognito_username_attributes": ["EMAIL"],
+  "aws_cognito_social_providers": [],
+  "aws_cognito_signup_attributes": ["EMAIL"],
+  "aws_cognito_mfa_configuration": "OFF",
+  "aws_cognito_mfa_types": ["SMS"],
+  "aws_cognito_password_protection_settings": {
+    "passwordPolicyMinLength": 8,
+    "passwordPolicyCharacters": []
+  },
+  "aws_cognito_verification_mechanisms": ["EMAIL"],
+  "aws_user_files_s3_bucket": "ycthegrid",
+  "aws_user_files_s3_bucket_region": "ca-central-1"
+}

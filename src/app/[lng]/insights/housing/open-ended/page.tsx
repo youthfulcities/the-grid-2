fad1139e31236210eceb573c9e7fabfb81a04e9d
Genna <gennamweber@gmail.com>
2024-11-26 15:43:00 -0500

    Make bubble chart larger

--- src/app/[lng]/insights/housing/open-ended/page.tsx
+++ src/app/[lng]/insights/housing/open-ended/page.tsx
@@ -1,5 +1,3 @@
-// @ts-nocheck
-
 'use client';
 
 import config from '@/amplifyconfiguration.json';
@@ -9,13 +7,27 @@ import Tooltip from '@/app/components/dataviz/TooltipChart';
 import Drawer from '@/app/components/Drawer';
 import Quote from '@/app/components/Quote';
 import { useDimensions } from '@/hooks/useDimensions';
-import { Button, Flex, Heading, Link, Text, View } from '@aws-amplify/ui-react';
+import { Flex, Heading, Text, View } from '@aws-amplify/ui-react';
 import { Amplify } from 'aws-amplify';
 import { downloadData } from 'aws-amplify/storage';
 import { uniqueId } from 'lodash';
 import { ReactNode, useEffect, useRef, useState } from 'react';
 
 Amplify.configure(config);
+
+interface DataItem {
+  nodes: Array<{
+    id: string;
+    quotes: string[];
+    value: number;
+  }>;
+  links: Array<{
+    id: string;
+    source: string;
+    target: string;
+    value: number;
+  }>;
+}
 
 const colors = ['red', 'green', 'yellow', 'pink', 'blue'];
 
@@ -56,8 +68,8 @@ const Interview = () => {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
   const [loading, setLoading] = useState(true);
   const [code, setCode] = useState('');
-  const [data, setData] = useState();
-  const [quotes, setQuotes] = useState([]);
+  const [data, setData] = useState<DataItem[]>();
+  const [quotes, setQuotes] = useState<string[]>([]);
 
   useEffect(() => {
     const loadData = async () => {
@@ -72,88 +84,85 @@ const Interview = () => {
   console.log(data);
 
   return (
-    <Container>
-      <View className='container padding' ref={containerRef}>
-        <Heading level={1}>
-          Housing Survey —{' '}
-          <span className='highlight'>Open-ended responses</span>
-        </Heading>
-        <div className='inner-container'>
-          <Heading level={3} color='font.inverse' marginBottom='xl' />
-          <Text>
-            Explore answers to this question: Tell us any other ideas you may
-            have about housing in Canada. What were your experiences like
-            finding housing? What are you concerned about in the future? What
-            would you like to see happen to solve the housing crisis in Canada?
-          </Text>
-        </div>
-        <BubbleChartJSON
-          data={data}
-          setQuotes={setQuotes}
-          setCode={setCode}
-          width={width}
-          tooltipState={tooltipState}
-          setIsDrawerOpen={setIsDrawerOpen}
-          setTooltipState={setTooltipState}
-        />
-      </View>
-      <Drawer
-        isopen={isDrawerOpen}
-        onOpen={() => setIsDrawerOpen(true)}
-        onClose={() => {
-          setIsDrawerOpen(false);
-        }}
-        tabText='Quotes'
-      >
-        {quotes?.length > 0 ? (
-          <Flex direction='column' paddingTop='xxl' paddingBottom='xxl'>
-            <Heading level={3} color='font.inverse' marginBottom={0}>
-              Quotes tagged with theme {code}
-            </Heading>
-            {quotes &&
-              quotes.map((item, index) => (
-                <Quote
-                  $color={
-                    colors[index % colors.length] as
-                      | 'red'
-                      | 'green'
-                      | 'yellow'
-                      | 'pink'
-                      | 'blue'
-                      | 'neutral'
-                      | undefined
-                  }
-                  key={uniqueId()}
-                  quote={item}
-                  left={index % 2 === 0}
-                />
-              ))}
-            <Text marginTop='xl'>
-              What to query quotes across all cities? Use the YDL Chatbot.
+    <View ref={containerRef} style={{ overflow: 'hidden' }}>
+      <Container>
+        <View className='short-container' paddingTop='xxxl'>
+          <Heading level={1}>
+            Housing Survey —{' '}
+            <span className='highlight'>Open-ended responses</span>
+          </Heading>
+          <View className='inner-container'>
+            <Heading level={3} color='font.inverse' marginBottom='xl' />
+            <Text>
+              Explore answers to this question: Tell us any other ideas you may
+              have about housing in Canada. What were your experiences like
+              finding housing? What are you concerned about in the future? What
+              would you like to see happen to solve the housing crisis in
+              Canada? This visualization is generated with the help of AI.
             </Text>
-            <Link href='/chatbot' target='_blank'>
-              <Button variation='primary' marginBottom='xl'>
-                Go to YDL Chatbot
-              </Button>
-            </Link>
-          </Flex>
-        ) : (
-          <>
-            <Text>Please click on a theme node to view quotes.</Text>{' '}
-          </>
+          </View>
+        </View>
+        <Flex justifyContent='center' paddingBottom='xxxl'>
+          <BubbleChartJSON
+            data={data}
+            setQuotes={setQuotes}
+            setCode={setCode}
+            width={width}
+            tooltipState={tooltipState}
+            setIsDrawerOpen={setIsDrawerOpen}
+            setTooltipState={setTooltipState}
+          />
+        </Flex>
+        <Drawer
+          isopen={isDrawerOpen}
+          onOpen={() => setIsDrawerOpen(true)}
+          onClose={() => {
+            setIsDrawerOpen(false);
+          }}
+          tabText='Quotes'
+        >
+          {quotes?.length > 0 ? (
+            <Flex direction='column' paddingTop='xxl' paddingBottom='xxl'>
+              <Heading level={3} color='font.inverse' marginBottom={0}>
+                Quotes tagged with theme {code}
+              </Heading>
+              {quotes &&
+                quotes.map((item, index) => (
+                  <Quote
+                    $color={
+                      colors[index % colors.length] as
+                        | 'red'
+                        | 'green'
+                        | 'yellow'
+                        | 'pink'
+                        | 'blue'
+                        | 'neutral'
+                        | undefined
+                    }
+                    key={uniqueId()}
+                    quote={item}
+                    left={index % 2 === 0}
+                  />
+                ))}
+            </Flex>
+          ) : (
+            <>
+              <Text>Please click on a theme node to view quotes.</Text>{' '}
+            </>
+          )}
+        </Drawer>
+        {tooltipState.position && (
+          <Tooltip
+            x={tooltipState.position.x}
+            content={tooltipState.content}
+            y={tooltipState.position.y}
+            group={tooltipState.group}
+            child={tooltipState.child}
+            minWidth={tooltipState.minWidth}
+          />
         )}
-      </Drawer>
-      {tooltipState.position && (
-        <Tooltip
-          x={tooltipState.position.x}
-          content={tooltipState.content}
-          y={tooltipState.position.y}
-          group={tooltipState.group}
-          child={tooltipState.child}
-          minWidth={tooltipState.minWidth}
-        />
-      )}
-    </Container>
+      </Container>
+    </View>
   );
 };
 

e19bdb769f2e2cb2839751a768d9fe01f7aef815
Genna <gennamweber@gmail.com>
2025-06-17 14:45:40 -0400

    Update middleware

--- src/middleware.js
+++ src/middleware.js
@@ -12,6 +12,29 @@ export const config = {
 };
 
 export function middleware(req) {
+  const env =
+    process.env.AMPLIFY_ENV ||
+    process.env.NEXT_PUBLIC_ENV ||
+    process.env.NODE_ENV;
+
+  // Restrict access to RAI in prod before release
+  if (env === 'prod' || env === 'staging') {
+    // Check if the request is for the restricted route.
+    if (
+      req.nextUrl.pathname.includes('/insights/real-affordability') &&
+      !req.nextUrl.pathname.includes('/grocery') &&
+      !req.nextUrl.pathname.includes('/coming-soon')
+    ) {
+      // Option 1: Redirect to a custom 403 page:
+      return NextResponse.redirect(
+        new URL('/insights/real-affordability/coming-soon', req.url)
+      );
+
+      // Option 2: Return a 403 status response:
+      // return new NextResponse('Forbidden', { status: 403 });
+    }
+  }
+
   let lng;
   if (req.cookies.has(cookieName))
     lng = acceptLanguage.get(req.cookies.get(cookieName).value);
@@ -36,24 +59,6 @@ export function middleware(req) {
     const response = NextResponse.next();
     if (lngInReferer) response.cookies.set(cookieName, lngInReferer);
     return response;
-  }
-
-  // Restrict access to RAI in prod before release
-  if (process.env.ENV === 'prod' || process.env.ENV === 'staging') {
-    // Check if the request is for the restricted route.
-    if (
-      req.nextUrl.pathname.includes('/insights/real-affordability') &&
-      !req.nextUrl.pathname.includes('/grocery') &&
-      !req.nextUrl.pathname.includes('/coming-soon')
-    ) {
-      // Option 1: Redirect to a custom 403 page:
-      return NextResponse.redirect(
-        new URL('/insights/real-affordability/coming-soon', req.url)
-      );
-
-      // Option 2: Return a 403 status response:
-      // return new NextResponse('Forbidden', { status: 403 });
-    }
   }
 
   return NextResponse.next();
